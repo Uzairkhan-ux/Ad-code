d657b3c12443ddcac9edbe1ce1aeefb98a1f0d07
Uzairkhan-ux <uzairkhan.me9@gmail.com>
2024-11-02 15:41:13 +0500

    Update ad.js

--- ad.js
+++ ad.js
@@ -1,7 +1,13 @@
+// Ad configuration
 atOptions = {
-    'key' : '8503b7f2d0f912e4b51ce7ebf4176b5f',
-    'format' : 'iframe',
-    'height' : 90,
-    'width' : 728,
-    'params' : {}
+    'key': '8503b7f2d0f912e4b51ce7ebf4176b5f',
+    'format': 'iframe',
+    'height': 90,
+    'width': 728,
+    'params': {}
 };
+
+// Dynamically load the invoke.js script
+var script = document.createElement('script');
+script.src = "//www.topcreativeformat.com/8503b7f2d0f912e4b51ce7ebf4176b5f/invoke.js";
+document.body.appendChild(script);
